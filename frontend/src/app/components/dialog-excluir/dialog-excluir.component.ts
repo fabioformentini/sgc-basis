933dcc1e9a5f4e7f2f068c23a9d5994b53914799
fabioformentini <fbformentini@gmail.com>
2022-03-17 11:00:29 -0300

    Add componente de dialog de exclusão

---
+++ frontend/src/app/components/dialog-excluir/dialog-excluir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-excluir',
+  templateUrl: './dialog-excluir.component.html',
+  styleUrls: ['./dialog-excluir.component.css']
+})
+export class DialogExcluirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
